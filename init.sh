39cbd05b451b7878ba10ebc053aca541e0724d27
seljydef <seljydef@gmail.com>
2018-06-09 07:47:45 +0000

    add.

---
+++ init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Create an admin user (you will be prompted to set username, first and last name before setting a password)
+fabmanager create-admin --app superset
+
+# Initialize the database
+superset db upgrade
+
+# Load some data to play with
+superset load_examples
+
+# Create default roles and permissions
+superset init
