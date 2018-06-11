a161d283c6b007630e698648fda20234f681fe6d
seljydef <seljydef@gmail.com>
2018-06-11 10:37:20 +0900

    fix.

--- init.sh
+++ init.sh
@@ -6,8 +6,5 @@ fabmanager create-admin --app superset
 # Initialize the database
 superset db upgrade
 
-# Load some data to play with
-superset load_examples
-
 # Create default roles and permissions
 superset init
